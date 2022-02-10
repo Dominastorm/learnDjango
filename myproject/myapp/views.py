c86b0a1b3dde5d177501568251d954ed48b11781
Dominastorm <dchawla228@gmail.com>
2022-02-10 23:23:50 +0530

    Passing variables into the html page

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -3,4 +3,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'name': 'Django',
+        'age': '18',
+        'nationality': 'indian'
+    }
+    return render(request, 'index.html', context)
