71598b567da9f1b209ce96ff1f512aaa1367d909
Dominastorm <dchawla228@gmail.com>
2022-02-12 19:05:31 +0530

    login + logout functionality

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -34,7 +34,24 @@ def register(request):
     else:
         return render(request, 'register.html')
         
-        
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info("Credendtials Invalid")
+            return redirect('login')
+    return render(request, 'login.html')        
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
 
 def counter(request):
     text = request.POST['text']
